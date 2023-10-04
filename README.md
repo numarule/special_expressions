841d02b64d39a0d6a51c00f90e3ea7166b06aff4
Numa <numarulettv@gmail.com>
2023-10-04 01:54:16 -0600

    WIP: Working on function for addressable named captures, currently not working

--- README.md
+++ README.md
@@ -1,8 +1,31 @@
-# NumaRule
-## Special Expressions
-Golang
+# Special Expressions
 
-Regular Expressions++
+Addressed named captures in regexp.
+
+Separator
+
+## Simple flat example:
+```go
+	input := "Player: vognod - Position: (56,45,11)"
+  // Simplest 1d version is just a `map[captureName]Value`
+	matchMap := CreateMatchMap(re, matches)
+  //...
+	fmt.Printf("matchMap[\"Player\"]: %v\n", matchMap["Player"])
+	fmt.Printf("matchMap[\"Position\"]: %v\n", matchMap["Position"])
+  //...
+```
+## Nested example:
+```go
+	input := "Player: vognod - Position: (56,45,11)"
+  // Simplest 1d version is just a `map[captureName]Value`
+	matchMap := CreateMatchMap(re, matches)
+  //...
+	fmt.Printf("matchMap[\"Player\"]: %v\n", matchMap["Player"])
+	fmt.Printf("matchMap[\"Position\"]: %v\n", matchMap["Position"])
+  //...
+```
+
+Primarily this is a small scope project while I get streaming setup, tested and operating well.
 
 ### Feature Ideas
 - Builder pattern for constructing regular expressions
--- main.go
+++ main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strings"
 )
-
-// Example of wanted query string - 'Player.Position.X'
 
 // Test data to get us working with regexps
 const identRe_fmt = "(?P<%s>[[:alpha:]]+[[:alnum:]]*)"
@@ -18,14 +17,72 @@ var zRe = fmt.Sprintf(numRe_fmt, "Z")
 var positionRe_fmt = fmt.Sprintf("(?P<Position>\\(\\s*%s\\s*,\\s*%s\\s*,\\s*%s\\s*\\))", xRe, yRe, zRe)
 var playerRe_fmt = fmt.Sprintf("(?P<Player>Player:\\s*%s\\s*-\\s*Position:\\s*%s)", nameRe, positionRe_fmt)
 
-//PROBLEM: Can't create a recursive type
-
-//Current thoughts
-//Interface? on addressable or j
-
-//Addressable == returns a string of its own full match and/or can be addressed into further
-
 type MatchMap map[string]string
+
+
+const ADDRESS_SEPARATOR = "."
+
+// Returns value for matched capture @ `address`
+// input := "Player: vognod - Position: (56,45,11)"
+// re := regexp.MustCompile(?P<Player>Player:\\s*(?P<Name>[[:alpha:]]+[[:alnum:]]*)\\s*-\\s*Position:\\s*(?P<Position>\\(\\s*(?P<X>[[:digit:]]+)\\s*,\\s*(?P<Y>[[:digit:]]+)\\s*,\\s*(?P<Z>[[:digit:]]+)\\s*\\)))
+//
+// address := "Player.Position.X"
+// fmt.Println(address) // "56"
+// Requires:
+// FindStringSubmatch - to parse out matches
+// FindStringSubmatchIndex - to parse out match ranges (start = 2n, end = 2n+2)
+// SubexpNames - Names of named expressions, in order of appearance in regex
+func GetValue(reStr string, input string, address string) (string, error) {
+	re := regexp.MustCompile(reStr)
+
+	//TODO: Opt, store matches and indicies in object to prevent rematching
+	matches := re.FindStringSubmatch(input)
+	if len(matches) <= 0 {
+		return "", fmt.Errorf("ERROR! No matches!\n")
+	}
+
+	indicies := re.FindStringSubmatchIndex(input)
+	if len(indicies) <= 0 {
+		return "", fmt.Errorf("ERROR! No indicies!\n")
+	}
+
+	captureNames := re.SubexpNames()
+	if len(captureNames) <= 0 {
+		return "", fmt.Errorf("ERROR! No named captures!\n")
+	}
+
+	//BUG Algorithm as is isn't working, and would match improper nesting
+	//  NEED to use stack of capture indicies for determining current path
+	//    Indicies are unique, unlike names
+	//    Reconstruct current address with the stack portions indexed into captureNames
+	currentAddressStack := make([]int, 0)
+	for i := range captureNames {
+		maybeAddress :=AddressStack_ToString(captureNames, append(currentAddressStack, i))
+		if !strings.HasPrefix(address, maybeAddress) {
+			//Not a match
+			continue
+		}
+		currentAddressStack = append(currentAddressStack, i)
+	}
+
+	if len(currentAddressStack) <= 0 {
+		return "", fmt.Errorf("ERROR! Address not found: '%s'\n", address)
+	}
+
+	currentIndex := currentAddressStack[len(currentAddressStack)-1]
+	match := matches[currentIndex]
+
+	return match, nil
+}
+
+func AddressStack_ToString(captureNames []string, stack []int) (r string) {
+	for _, addressSegment := range stack {
+		r += captureNames[addressSegment] + ADDRESS_SEPARATOR
+	}
+	r = strings.TrimSuffix(r, ADDRESS_SEPARATOR)
+	return
+}
+
 
 func CreateMatchMap(re *regexp.Regexp, matches []string) MatchMap {
 	matchMap := make(MatchMap, re.NumSubexp())
@@ -39,18 +96,12 @@ func main() {
 	fmt.Println("Hello World")
 
 	input := "Player: vognod - Position: (56,45,11)"
-	re := regexp.MustCompile(playerRe_fmt)
+	reStr := playerRe_fmt
 
-	matches := re.FindStringSubmatch(input)
-	if len(matches) <= 0 {
-		panic(fmt.Errorf("ERROR! No matches!!!\n"))
+	playerY, err := GetValue(reStr, input, "Player.Position.Y")
+	if err != nil {
+		panic(err)
 	}
 
-	fmt.Printf("Matches: %#v\n", matches)
-	fmt.Printf("SubexpNames: %#v\n", re.SubexpNames())
-	fmt.Printf("SubexpNameIndexes: %#v\n", re.NumSubexp())
-
-	matchMap := CreateMatchMap(re, matches)
-
-	fmt.Printf("matchMap: %#v\n", matchMap)
+	fmt.Printf("Player.Position.Y: '%s'\n", playerY)
 }
